32919b7f0123016c744044701b1b5d5102db6688
darideveloper <darideveloper@gmail.com>
2023-05-26 17:43:08 -0600

    fix (credentials): return empty list where no debug users

--- credentials.py
+++ credentials.py
@@ -6,4 +6,8 @@ API_HOST = os.getenv("API_HOST")
 TOKEN = os.getenv("TOKEN")
 PORT = os.getenv("PORT")
 CHROME_PATH = os.getenv("CHROME_PATH")
-DEBUG_USERS = os.getenv("DEBUG_USERS").split(",")
+DEBUG_USERS = os.getenv("DEBUG_USERS")
+if DEBUG_USERS:
+    DEBUG_USERS = DEBUG_USERS.split(",")
+else:
+    DEBUG_USERS = []
